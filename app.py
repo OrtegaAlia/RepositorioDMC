bfac374f34fb0235ac55d38bb10b128943b69aae
OrtegaAlia <ortegaalia13@gmail.com>
2026-05-22 18:01:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ st.title("Mi primera aplicación en Python")
 
 st.subheader("Módulo 1 - Fundamentos de Programación")
 
+URL_LOGO = "https://python.org"
 st.image(URL_LOGO, width=200, caption="Python DMC")
