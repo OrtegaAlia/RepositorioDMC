2aeddb07075f1529f4a8f510a30089b07d578195
OrtegaAlia <ortegaalia13@gmail.com>
2026-05-22 18:05:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 st.title("Mi primera aplicación en Python")
 
-st.subheader("Módulo 1 - Fundamentos de Programación")
-
 URL_LOGO = "https://python.org"
 
 st.image(URL_LOGO, width=200, caption="Python DMC")
 
 st.write("Alia Ortega Alvarado")
+
+st.subheader("Módulo 1 - Fundamentos de Programación")
