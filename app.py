31b2d0f7469ca8eb050781cadf35395d3be24ab9
OrtegaAlia <ortegaalia13@gmail.com>
2026-05-22 17:38:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title("Módulo 1 - Fundamentos de Programación")
+st.title("Mi primera aplicación en Python")
 
-st.subheader("Trabajo Práctico")
+st.subheader("Módulo 1 - Fundamentos de Programación")
+
+
