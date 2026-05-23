c025d38fef8800142185a3acc3eb254cac7383e4
OrtegaAlia <ortegaalia13@gmail.com>
2026-05-22 22:06:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,24 @@ if Ejercicio == "Ejercicio 1":
   st.write("Bienvenido Ejercicio 1")
   st.image("Flujo de Caja.png")
   if "lista_movimientos" not in st.session_state:
-    st.session_state.lista_movimientos = []
+  st.session_state.lista_movimientos = []
+st.markdown("""
+Este módulo permite registrar de forma dinámica tus movimientos financieros. 
+Cada registro captura el concepto, tipo de operación y monto. Las transacciones se 
+almacenan de manera secuencial para procesar balances económicos en tiempo real.
+""")
+st.subheader("📥 Registro de Nuevo Movimiento")
+col_concepto, col_tipo, col_valor = st.columns(3)
+
+with col_concepto:
+    concepto = st.text_input("Concepto:", placeholder="Ej. Pago de Consultoría", key="input_concepto")
+
+with col_tipo:
+    tipo_movimiento = st.selectbox("Tipo de Movimiento:", ["Ingreso", "Gasto"], key="sb_tipo")
+
+with col_valor:
+    valor = st.number_input("Valor (S/):", min_value=0.0, step=10.0, format="%.2f", key="num_valor")
+
 
 elif Ejercicio == "Ejercicio 2":
   st.write("Bienvenido Ejercicio 2")
