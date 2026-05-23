9f1677503eaf8e0f17109d5651c2be55f0c1c77b
OrtegaAlia <ortegaalia13@gmail.com>
2026-05-22 21:25:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ st.sidebar.image("LOGO.png")
 
 st.sidebar.subheader("Módulo: Python para Analítica de Datos (Python DMC)")
 
+st.sidebar.write("**Nombre Completo:** Alia Ortega Alvarado")
+st.sidebar.write("**Especialidad / Rol:** Analista de Contabilidad")
+st.sidebar.write("**Año Académico:** 2026")
+
 Ejercicio = st.sidebar.selectbox("Seleccione un Ejercicio", ["Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"] )
 
 if Ejercicio == "Ejercicio 1":
