de95a19f02183114605ccee2aedd3d57f7e3604f
OrtegaAlia <ortegaalia13@gmail.com>
2026-05-22 21:39:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ st.sidebar.write("**Nombre Completo:** Alia Ortega Alvarado")
 st.sidebar.write("**Especialidad / Rol:** Analista de Contabilidad")
 st.sidebar.write("**Año Académico:** 2026")
 
-st.sidebar.markdown("Este proyecto es una aplicación interactiva diseñada para centralizar flujos de trabajo clave. Permite realizar el control de **flujos de caja**, 
-la gestión avanzada con **NumPy**, así como la ejecución de cálculos financieros y operaciones **CRUD** completas mediante módulos 
+st.sidebar.markdown("Este proyecto es una aplicación interactiva diseñada para centralizar flujos de trabajo clave. Permite realizar el control de **flujos de caja**, la gestión avanzada con **NumPy**, así como la ejecución de cálculos financieros y operaciones **CRUD** completas mediante módulos 
 y librerías externas de Python.")
 
 st.sidebar.write("Streamlit, NumPy, arrays, DataFrame y Pandas")
