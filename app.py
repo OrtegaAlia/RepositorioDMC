8b93537ae3e698c9c763333794c81a1889e31a49
OrtegaAlia <ortegaalia13@gmail.com>
2026-05-22 21:23:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ st.write("Elaborado por: Alia Ortega")
 
 st.sidebar.image("LOGO.png")
 
+st.sidebar.subheader("Módulo: Python para Analítica de Datos (Python DMC)")
+
 Ejercicio = st.sidebar.selectbox("Seleccione un Ejercicio", ["Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"] )
 
 if Ejercicio == "Ejercicio 1":
@@ -28,21 +30,4 @@ elif Ejercicio == "Ejercicio 4":
   st.write("Bienvenido Ejercicio 4")
   st.image("Crud.png")
 
-st.subheader("Módulo: Python para Analítica de Datos (Python DMC)")
-
-st.header("📋 Información del Proyecto")
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.markdown("### 🧑‍💻 Información del Estudiante")
-    st.write("**Nombre Completo:** Alia Ortega Alvarado")
-    st.write("**Especialidad / Rol:** Analista de Contabilidad")
-    st.write("**Año Académico:** 2026")
-
-with col2:
-    st.markdown("### 📋 Descripción del Proyecto")
-    st.markdown("""Este proyecto es una aplicación interactiva diseñada para centralizar flujos de trabajo clave. 
-                   Permite realizar el control de **flujos de caja**, la gestión avanzada de ingresos y gastos, 
-                   así como la ejecución de cálculos financieros y operaciones **CRUD** completas mediante módulos y librerías externas de Python.""")
 
