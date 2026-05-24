496a19db851e8d616227a0c78aaf88ffd3ecd8bc
OrtegaAlia <ortegaalia13@gmail.com>
2026-05-23 20:37:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -169,11 +169,14 @@ elif Ejercicio == "Ejercicio 3":
         st.session_state.historico_funciones = pd.DataFrame(columns=[
         "Función Ejecutada", "Parámetro 1", "Parámetro 2", "Parámetro 3", "Resultado Final"
         ])
-    st.markdown("Este módulo conecta la interfaz gráfica de Streamlit con algoritmos alojados en el archivo modular independiente `libreria_funciones_proyecto1.py`. 
-    Al parametrizar las variables de entrada y activar el proceso, se ejecuta la rutina analítica seleccionada y se guarda un registro de auditoría 
-    en una tabla histórica matricial.")
+    st.markdown("""
+Este módulo conecta la interfaz gráfica de Streamlit con algoritmos alojados en el archivo 
+modular independiente `libreria_funciones_proyecto1.py`. Al parametrizar las variables de entrada 
+y activar el proceso, se ejecuta la rutina analítica seleccionada y se guarda un registro de auditoría 
+en una tabla histórica matricial.
+""")
 
-    st.markdown("---")
+   st.markdown("---")
 
 
 elif Ejercicio == "Ejercicio 4":
